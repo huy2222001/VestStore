8c380cd618cfb90c181525db8c6cb24dfe1e3557
huynh2222001@gmail.com <huynh2222001@gmail.com>
2022-06-18 15:16:01 +0700

    Add project files.

---
+++ VestStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VestStore.Startup))]
+namespace VestStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
